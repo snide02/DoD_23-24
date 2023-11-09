b0a1dac2bff0ae3d2d790b0d174f40a197a6ed4c
snide02 <michaelsnider2002@gmail.com>
2023-11-09 14:43:55 -0500

    Nobleman base game
    Added Class for the base music game.

---
+++ Source/Gameplay/World/Nobleman/baseMusicGame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoD_23-24.Source.Gameplay.World.Nobleman
+{
+    internal class baseMusicGame
+{
+}
+}
