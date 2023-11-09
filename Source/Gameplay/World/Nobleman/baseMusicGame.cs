df1db702f6f7435dda19d72e06a3a87642a92389
Chandler <cjoynes@umich.edu>
2023-11-09 15:49:07 -0500

    Outline
    A basic outline of our base class.

--- Source/Gameplay/World/Nobleman/baseMusicGame.cs
+++ Source/Gameplay/World/Nobleman/baseMusicGame.cs
@@ -2,11 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DoD_23_24.Source.Gameplay.World.Nobleman
 {
     internal class baseMusicGame
     {
+        int bpm = 2;
+        Level musicGame;
+        Player player;
+
+        //Moving Object Function
+
+        //Door Function
+
+        //Object Path Function
+
+        //Start Song Function
+            //Start Timer
+
+        //Get Time Function
+
+        //Health Function
+
+        //Update Function
+
+        //Draw Function (Square)
+
+        //Room Creation Function
+            //Draw in the room
     }
 }
